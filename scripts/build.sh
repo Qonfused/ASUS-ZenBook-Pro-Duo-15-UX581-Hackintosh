cc33ded1d09bb337bc4b6c4d129469337f1c9b5d
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2024-07-24 15:10:25 -0500

    Update project to use new OCE-Build tool
    Partially resolves #6.

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,20 +11,6 @@
 # Change CWD for imports
 __PWD__=$(pwd); cd "$(realpath "$(dirname "${BASH_SOURCE[0]}")"/../)"
 
-source ./scripts/lib/constants.sh
-source ./scripts/lib/oce-build/lib/macros.sh
-
 # Run build script
-if printf '%s\n' "$@" | grep -Fxq -- '--UX581GV'; then
-  # Patch for UX581GV
-  bash ./scripts/lib/oce-build/build.sh -c "$CONFIG" --UX581GV
-else
-  # Patch for UX581LV
-  bash ./scripts/lib/oce-build/build.sh -c "$CONFIG" --UX581LV
-fi
-
-# Patch SMBIOS serial data
-if printf '%s\n' "$@" | grep -Fxq -- '--skip-serial'; then
-  echo "Skipping SMBIOS serial generation..."
-elif [[ ! -f ./src/.serialdata ]]; then bash ./scripts/lib/gen-serial.sh; fi
-bash ./scripts/lib/patch-serial.sh
+curl -sL https://raw.githubusercontent.com/Qonfused/OCE-Build/main/ci/bootstrap.sh \
+  | bash -s build "$@"
